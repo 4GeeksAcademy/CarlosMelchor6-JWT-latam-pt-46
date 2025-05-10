394daf06c69a2c679121711b9c294274778dc442
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-10 01:07:45 +0000

    finish

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<span className="navbar-brand mb-0 h1 fs-3 text-primary fw-bolder">
-					<i className="fa-solid fa-book"></i>
-					Diary.com
-				</span>
+				<Link className="link-underline-primary link-offset-2" to="/">
+					<span className="navbar-brand mb-0 h1 fs-3 text-primary fw-bolder" >
+						<i className="fa-solid fa-book"></i>
+						Diary.com
+					</span>
+				</Link>
 			</div>
 		</nav>
 	);
